405778c885119c4ba8d13b051803a7769a483106
Shannon Young <shannon.ari1@gmail.com>
2020-09-11 22:50:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-      "Fizz" # Go fizz
+def fizzbuzzdo
+  it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+
+    expect(fizz_3).to eq("Fizz")
+
     end
 end
